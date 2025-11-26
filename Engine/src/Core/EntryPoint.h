2fac6342e0824fa0c2f7c45647a969e4dd1f131f
FilipT03 <filipsombor@gmail.com>
2025-11-26 19:56:10 +0100

    Add logging

--- Engine/src/Core/EntryPoint.h
+++ Engine/src/Core/EntryPoint.h
@@ -6,6 +6,10 @@ extern ft::Application* ft::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	ft::Log::Init();
+	FT_CORE_ERROR("Error message");
+	FT_INFO("Info from client!");
+
 	auto app = ft::CreateApplication();
 	app->Run();
 	delete app;
