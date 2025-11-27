5c55335f1d4077ae5835039a475517326a4695ab
FilipT03 <filipsombor@gmail.com>
2025-11-27 21:14:33 +0100

    Add time and timer

---
+++ Engine/src/Core/Time.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Core/Core.h"
+
+namespace ft {
+
+	class Time
+	{
+	public:
+		static double FT_API DeltaTime() { return s_DeltaTime; }
+		static void SetDeltaTime(double time) { s_DeltaTime = time; }
+		
+		static double FT_API TotalTime() { return s_Time; }
+		static void Reset() { s_Time = 0; s_DeltaTime = 0; }
+		static void UpdateTime(double newTime) 
+		{
+			s_DeltaTime = newTime - s_Time;
+			s_Time = newTime;
+		}
+
+	private:
+		inline static double s_DeltaTime = 0;
+		inline static double s_Time = 0;
+	};
+
+}
+
---
+++ Engine/src/Core/Timer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Core/Core.h"
+#include <chrono>
+
+namespace ft {
+
+	class Timer
+	{
+	public:
+		Timer() { Reset(); }
+		
+		void Reset() 
+		{
+			m_Start = std::chrono::high_resolution_clock::now();
+		}
+
+		/// <summary>
+		///		Get time since initiliazation in seconds.
+		/// </summary>
+		double Elapsed() const
+		{
+			return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_Start).count() * 0.001 * 0.001;
+		}
+
+		double ElapsedMiliseconds() const
+		{
+			return Elapsed() * 1000.0;
+		}
+
+	private:
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+	};
+}
+
